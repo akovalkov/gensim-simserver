b9dfcaa7ca048d7839f9e955249a45f53b0debd7
ese <ese@spdevtools01.stratus.northernlight.com>
2020-06-26 08:04:32 -0400

    Support Python 3

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     install_requires = [
         'gensim >= 0.8.5',
         'Pyro4 >= 4.8',
-        'sqlitedict >= 1.0.8',
+        'sqlitedict >= 1.6.0',
     ],
 
     include_package_data = True,
